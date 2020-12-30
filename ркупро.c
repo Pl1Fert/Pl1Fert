8f4f31003b781fb496d07cea21e72caa858c0c66
Alexey Rusel <59646567+Pl1Fert@users.noreply.github.com>
2020-12-30 15:00:13 +0300

    Update ркупро.c

--- ркупро.c
+++ ркупро.c
@@ -3,31 +3,31 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-enum Types {Int,Float};
-void sum(enum Types typ,int kl, ...)
+enum Types { Int, Float };
+void sum(enum Types typ, int kl, ...)
 {
     int sumI = 0;
-    float sumF = 0;
-    switch(typ)
+    double sumF = 0;
+    switch (typ)
     {
         case Int:
             va_list p;
             va_start(p, kl);
             for (int i = 0; i < kl; i++)
             {
-                sumI += va_arg(p,int);
+                sumI += va_arg(p, int);
             }
             *va_arg(p, int*) = sumI;
-            va_end(p)
-            break;
+            va_end(p);
+                break;
         case Float:
             va_list pp;
-            va_start(pp, double);
+            va_start(pp, kl);
             for (int i = 0; i < kl; i++)
             {
-                sumF += va_arg(pp,double);
+                sumF += va_arg(pp, double);
             }
-            *va_arg(p, double*) = sumF;
+            *va_arg(pp, double*) = sumF;
             va_end(pp);
             break;
     }
@@ -35,12 +35,12 @@ void sum(enum Types typ,int kl, ...)
 
 int main()
 {
-    setlocale(LC_ALL,"");
+    setlocale(LC_ALL, "");
     int summaInt;
     double summaFloat;
     sum(Int, 3, 54, 45, 56, &summaInt);
-    sum(Float, 3, (double)54.23, (double)466.4,(double)1.4, &summaFloat);
+    sum(Float, 3, (double)54.23, (double)466.4, (double)1.4, &summaFloat);
     printf("%d  %.3lf\n", summaInt, summaFloat);
-    
+
     return 0;
 }
